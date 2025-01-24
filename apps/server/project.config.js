4116175579608712a26a5681f5baa387ba119c92
丁少华 <960423114@qq.com>
2025-01-25 00:33:19 +0800

    fix: 更新vip账号

--- apps/server/project.config.js
+++ apps/server/project.config.js
@@ -2,7 +2,7 @@ const config = {
   uin: "869043928", // QQ号
   // 登录QQ音乐后查看Cookie,复制其中的 qm_keyst 字段到此
   qm_keyst:
-    "Q_H_L_63k3N8Ydcsi57WGO_-0WqNHzI2n7QOcEaKLJkFoQIXHlbP4ok2I9_IME1AsMWMl8A1u6e3eGhLLgtELW8gvPUBQ",
+    "Q_H_L_63k3N0LoZ07CDORV_l4a3-zJvvsrvgqnnESEQTlZcgCYKOvPAnal1TwEmMcVg3NVeuN4C-s1Jt6mkmeDanrqOCA",
 };
 
 export default config;
