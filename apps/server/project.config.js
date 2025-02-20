2e5c158b97dc8ec2b7a33951221d6d558055b917
丁少华 <dingshaohua@douyuxingchen.com>
2025-02-20 14:08:26 +0800

    Update project.config.js

--- apps/server/project.config.js
+++ apps/server/project.config.js
@@ -2,7 +2,7 @@ const config = {
   uin: "869043928", // QQ号
   // 登录QQ音乐后查看Cookie,复制其中的 qm_keyst 字段到此
   qm_keyst:
-    "Q_H_L_63k3NcBj1XcokaQGrmnVgKzmQyvXMZxi2V2hXUGVlA0vmCHo1iQ14Rt_9Kzyhcx92Y-vUpEssITfwHCuNvgxM7A",
+    "Q_H_L_63k3NSERf3oqOWdUcSwRY31bk1ZhPzW9Iq46J81ZSD3LAkbBy96NENaK5mZgulPb-whgT9P8cU1UsY50z6ay4Og",
 };
 
 export default config;
